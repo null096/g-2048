a710f6626937b5d2170495103a05a94777c6de71
nodejs <kanaradze223@gmail.com>
2019-03-14 12:37:48 +0200

    Updates

---
+++ precache-manifest.e8d5c89be86fff96230a8a8ef8bde456.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2cdc77082341d56e2408",
+    "url": "/null096/g-2048/static/js/runtime~main.79a0199c.js"
+  },
+  {
+    "revision": "8a1263e9ef7b70e14903",
+    "url": "/null096/g-2048/static/js/main.33b7f09a.chunk.js"
+  },
+  {
+    "revision": "aaddfa95988ad7eaba07",
+    "url": "/null096/g-2048/static/js/2.951a93fa.chunk.js"
+  },
+  {
+    "revision": "8a1263e9ef7b70e14903",
+    "url": "/null096/g-2048/static/css/main.c146c99d.chunk.css"
+  },
+  {
+    "revision": "7d75d7d8d0ae4b6194c9bde6ccca8f52",
+    "url": "/null096/g-2048/index.html"
+  }
+];
